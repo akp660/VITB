bf3aa5ab6e28f3b7ae6271b20b475704144d0133
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-07-08 20:32:51 +0530

    added auto day selected picker

--- app/src/main/java/com/abhijeet/vitb/loading_screen.java
+++ app/src/main/java/com/abhijeet/vitb/loading_screen.java
@@ -2,6 +2,7 @@ package com.abhijeet.vitb;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -15,6 +16,19 @@ public class loading_screen extends Activity {
         Intent iHome = new Intent(loading_screen.this,MainActivity.class);
 
         new Handler().postDelayed(() -> startActivity(iHome),1500);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            // Start the main activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            // Continue with your normal flow
+            // Set the content view, initialize views, etc.
+            setContentView(R.layout.activity_loading_screen);
+            // Other initialization or setup code...
+        }
+
+        finish();
 
     }
 
