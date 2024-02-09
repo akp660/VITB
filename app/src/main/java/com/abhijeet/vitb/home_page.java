f4bf9a6b3fa3e42f08521d6dc20e4254b9a3a0bc
Vibhor Mathur <matvibhor24@gmail.com>
2024-02-09 20:13:55 +0530

    created - Tab Layout on home screen (basic implementation)

--- app/src/main/java/com/abhijeet/vitb/home_page.java
+++ app/src/main/java/com/abhijeet/vitb/home_page.java
@@ -1,14 +1,70 @@
 package com.abhijeet.vitb;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class home_page extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    MainAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.viewpager);
+
+//        String mess = getResources().getString(R.string.Mess);
+        adapter = new MainAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new Mess(), "Mess");
+        adapter.AddFragment(new Mayuri(),"Mayuri's");
+        adapter.AddFragment(new Underbely(), "Under Belly");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private class MainAdapter extends FragmentPagerAdapter {
+        ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        public void AddFragment(Fragment fragment, String s){
+            fragmentArrayList.add(fragment);
+            stringArrayList.add(s);
+        }
+        public MainAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentArrayList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentArrayList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return stringArrayList.get(position);
+        }
     }
 }
