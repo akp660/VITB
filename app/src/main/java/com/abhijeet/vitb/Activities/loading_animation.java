21ae7e0f573e40a587960a54ff732920f1330b86
abhijeet <abhijeetpandey.dhn@gmail.com>
2024-04-13 21:40:05 +0530

    updated miniour ui design

---
+++ app/src/main/java/com/abhijeet/vitb/Activities/loading_animation.java
@@ -0,0 +1,26 @@
+package com.abhijeet.vitb.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.abhijeet.vitb.R;
+
+public class loading_animation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loading_animation);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
